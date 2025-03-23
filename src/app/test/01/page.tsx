ac43f00d8ff052af0bfea48c9cb7adb9a0d16fd3
Supakorn Panyasodaban <supakronnn55@gmail.com>
2025-03-23 19:21:37 +0700

    feat: add InterviewCard

--- src/app/test/01/page.tsx
+++ src/app/test/01/page.tsx
@@ -6,31 +6,24 @@ import HomePage from "@/components/HomePage";
 import CardPanel from "@/components/CardPanel";
 import axios from "axios";
 import Header from "@/components/Header";
+import InterviewsCard from "@/components/InterviewCard";
+
+const mockInterviews = [
+  {
+    companyName: "Google",
+    bookingDate: "2021-10-12",
+  },
+  {
+    companyName: "Facebook",
+    bookingDate: "2021-10-15",
+  },
+  {
+    companyName: "Amazon",
+    bookingDate: "2021-10-18",
+  },
+];
 
 export default function Home() {
-  const [companies, setCompanies] = useState<CompanyModel[]>([]); // Store all companies
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    // Fetch all company data from the API
-    const fetchCompanies = async () => {
-      try {
-        const response = await axios.get<CompaniesApi>(
-          `${process.env.BASE_API_URL}companies` // API endpoint
-        );
-        if (response.data.success) {
-          setCompanies(response.data.data as CompanyModel[]); // Assuming the API returns an array
-        }
-      } catch (error) {
-        console.error("Error fetching company data:", error);
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    fetchCompanies();
-  }, []);
-
   return (
     <div>
       <Topmenu />
@@ -43,14 +36,19 @@ export default function Home() {
         buttonType="none"
       />
 
-      <div className="flex flex-col items-center gap-4 flex-grow">
-        {loading ? (
-          <p>Loading...</p>
-        ) : companies.length > 0 ? (
-          <CardPanel companies={companies} /> // Use Cardpanel here
-        ) : (
-          <p>No company data available</p>
-        )}
+      <div className="w-screen bg-white p-4">
+        <div className="bg-c2 p-6 space-y-4 md:space-y-6 m-6 border-2 border-storke rounded-xl">
+          <div className="font-bold text-center text-2xl md:text-3xl">My Interviews</div>
+          {mockInterviews.map((interview, index) => (
+            <InterviewsCard
+              key={index}
+              companyName={interview.companyName}
+              bookingDate={interview.bookingDate}
+              onEdit={() => console.log("Edit")}
+              onRemove={() => console.log("Remove")}
+            />
+          ))}
+        </div>
       </div>
     </div>
   );
---
+++ src/components/InterviewCard.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+import { Building, Calendar } from "lucide-react";
+
+interface BookingCardProps {
+  companyName: string;
+  bookingDate: string;
+  onEdit: () => void;
+  onRemove: () => void;
+}
+
+export default function InterviewCard({
+  companyName,
+  bookingDate,
+  onEdit,
+  onRemove,
+}: BookingCardProps) {
+  return (
+    <div className="flex flex-col md:flex-row justify-between items-center p-6 border-storke border-1 bg-white rounded-lg shadow-md hover:shadow-lg hover:-translate-1 transition duration-300 ease-in-out">
+      {/* Company Info */}
+      <div className="flex flex-col items-center space-y-3">
+        <div className="flex items-center space-x-3">
+          <Building size={20} />
+          <h2 className="card-title text-lg md:text-2xl font-bold">
+            {companyName}
+          </h2>
+        </div>
+        <div className="flex items-center space-x-3">
+          <Calendar size={20} />
+          <h2 className="card-title text-sm md:text-base text-gray-500">
+            {bookingDate}
+          </h2>
+        </div>
+      </div>
+
+      {/* Buttons */}
+      <div className="flex flex-col md:flex-row space-x-3 md:space-x-4 gap-3 mt-3">
+        <button
+          onClick={onEdit}
+          className="px-3 md:px-6 py-1 md:py-2 m-0 border border-storke rounded-md hover:bg-gray-200 transition duration-300"
+        >
+          <div className="text-black text-sx md:text-base">Edit Booking</div>
+        </button>
+        <button
+          onClick={onRemove}
+          className="px-3 md:px-6 py-1 md:py-2 border border-storke rounded-md  bg-bgred hover:bg-red-400 hover:border-black border-1 transition duration-300"
+        >
+          <div className="text-black text-sm md:text-base">Remove Booking</div>
+        </button>
+      </div>
+    </div>
+  );
+}
