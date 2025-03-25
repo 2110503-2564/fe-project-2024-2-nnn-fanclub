fd2c71953d2149a1d4e51f95591af3c7c5c60787
Wasawat Junnasaksri <ice7533678@gmail.com>
2025-03-25 12:44:56 +0700

    feat: add Pagination for User use

--- src/app/booking/page.tsx
+++ src/app/booking/page.tsx
@@ -1,7 +1,6 @@
-"use client";
+"use client"
 import CardPanel from "@/components/CardPanel";
 import Header from "@/components/Header";
-import TopMenu from "@/components/TopMenu";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { useSession } from "next-auth/react";
@@ -11,13 +10,18 @@ export default function booking() {
 
   const [companies, setCompanies] = useState<CompanyModel[]>([]); // Store all companies
   const [loading, setLoading] = useState(true);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);  
 
   useEffect(() => {
-    const fetchCompanies = async () => {
+    const fetchCompanies = async (page: number) => {
       try {
-        const response = await axios.get(`${process.env.BASE_API_URL}/companies`);
+        const response = await axios.get<CompaniesApi>(
+          `${process.env.BASE_API_URL}/companies?page=${page}`
+        );
         if (response.data.success) {
           setCompanies(response.data.data as CompanyModel[]);
+          setTotalPages(response.data.pagination?.maxPage as number);
         }
       } catch (error) {
         console.error("Error fetching company data:", error);
@@ -26,12 +30,16 @@ export default function booking() {
       }
     };
 
-    fetchCompanies();
-  }, []);
+    fetchCompanies(currentPage);
+  }, [currentPage]);
+
+  const handlePageChange = (page: number) => {
+    setLoading(true);
+    setCurrentPage(page);
+  };
 
   return (
     <div>
-      <TopMenu/>
       <Header
         header="Participating Companies"
         description="Browse all companies participating in our online job fair"
@@ -43,7 +51,12 @@ export default function booking() {
         {loading ? (
           <p>Loading...</p>
         ) : companies.length > 0 ? (
-          <CardPanel companies={companies} /> // Use Cardpanel here
+          <CardPanel 
+            companies={companies} 
+            currentPage={currentPage} 
+            totalPages={totalPages} 
+            onPageChange={handlePageChange} 
+          /> // Use Cardpanel here
         ) : (
           <div className="text-center text-xl text-red-700">
             No company data available
