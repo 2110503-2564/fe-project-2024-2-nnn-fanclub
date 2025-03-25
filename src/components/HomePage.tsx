2da0c3bd2b8a6d0bda46f33c8a80677d27948974
Supakorn Panyasodaban <supakronnn55@gmail.com>
2025-03-25 20:53:28 +0700

    fix: HomePage gradient

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -23,7 +23,7 @@ function TypewriterText({ text }: { text: string }) {
 
 export default function HomePage() {
   return (
-    <div className="bg-c2 flex flex-col items-center w-screen h-auto pb-3">
+    <div className="bg-linear-to-r/srgb from-white to-[#e1d9c7] rounded-b-xl flex flex-col items-center w-screen h-auto pb-3">
       {/* Header Section */}
       <header className="text-center py-6">
         <h1 className="text-2xl md:text-5xl font-bold mt-3 md:mt-5">
